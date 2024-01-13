1e266ab3555e615473e7ed6c7cf9ea2fbb32bff4
crisncris0000 <christopherrivera384@gmail.com>
2024-01-13 12:47:52 -0500

    Worked on the handler for resolution subject and updated user model

--- be-app/internal/handlers/resolutionsubject.go
+++ be-app/internal/handlers/resolutionsubject.go
@@ -1,1 +1,33 @@
 package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/gmadharh/hack-united/be-app/internal/models"
+)
+
+type ResolutionSubjectHandler struct {
+	DB *models.ResolutionSubjectModelImpl
+}
+
+func NewResolutionHandler(db *models.ResolutionSubjectModelImpl) *ResolutionSubjectHandler {
+	return &ResolutionSubjectHandler{DB: db}
+}
+
+func (resolutionHandler *ResolutionSubjectHandler) GetAllSubjects(context *gin.Context) {
+
+	subjects, err := resolutionHandler.DB.GetAllSubjects()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message": "Error retrieving subjects",
+			"error":   err,
+		})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{
+		"subjects": subjects,
+	})
+}
