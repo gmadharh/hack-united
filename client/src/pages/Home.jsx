754685c82647de2bfcd9744e412dfba56686d92a
Gevin <gevinmadharha@gmail.com>
2024-01-13 15:09:01 -0500

    fixed error on homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
-import { useUsers } from "../controllers/fetchUsers";
+import { useContext } from 'react'
+//import { useUsers } from "../controllers/fetchUsers";
 
-import { AuthContext } from "../contexts/AuthContext";
+import { AuthContext } from '../contexts/AuthContext'
 
 const Home = () => {
-//   const { name } = useContext(AuthContext);
+  //   const { name } = useContext(AuthContext);
 
   return (
     <div className="container mx-auto mt-24">
       <h2 className="text-5xl text-white font-bold heading">Hello World!</h2>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
