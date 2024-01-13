e9e8602bcd29a706548c5a2d5a7469bcd4f4d032
Shishir <shishir3324kumar@gmail.com>
2024-01-13 12:20:17 +0530

    Spinning wheel modified in the home page

--- client/src/components/Wheel.js
+++ client/src/components/Wheel.js
@@ -1,41 +1,47 @@
 import React, { useState } from 'react'
 import { Wheel } from 'react-custom-roulette'
-
-const data = [
-  { option: '0', style: { backgroundColor: 'green', textColor: 'black' } },
-  { option: '1', style: { backgroundColor: 'white' } },
-  { option: '2' },
-]
+import { generateData } from '../generateData'
 
 const WheelComponent = () => {
-  const [mustSpin, setMustSpin] = useState(false)
+  const [spinStatus, setSpinStatus] = useState(false)
   const [prizeNumber, setPrizeNumber] = useState(0)
-  const [wheelData, setWheelData] = useState([
-    { option: '0', style: { backgroundColor: 'green', textColor: 'black' } },
-    { option: '1', style: { backgroundColor: 'white' } },
-    { option: '2' },
-  ])
+  const [wheelData, setWheelData] = useState(generateData(10))
 
   const handleSpinClick = () => {
-    if (!mustSpin) {
-      const newPrizeNumber = Math.floor(Math.random() * data.length)
+    if (!spinStatus) {
+      const newPrizeNumber = Math.floor(Math.random() * wheelData.length)
       setPrizeNumber(newPrizeNumber)
-      setMustSpin(true)
+      setSpinStatus(true)
+    } else {
+      console.log('Already spinning!')
+      // setSpinStatus(false)
     }
   }
 
   return (
     <>
       <Wheel
-        mustStartSpinning={mustSpin}
+        mustStartSpinning={spinStatus}
         prizeNumber={prizeNumber}
         data={wheelData}
+        outerBorderWidth={5}
+        outerBorderColor="#adadad"
+        radiusLineWidth={0}
+        // spinDuration={0.9}
         onStopSpinning={() => {
-          setMustSpin(false)
+          console.log('stopped spinning!', prizeNumber)
+          setSpinStatus(false)
+        }}
+        disableInitialAnimation={true}
+        startingOptionIndex={0}
+        pointerProps={{
+          style: {
+            color: 'green',
+          }
         }}
       />
-      <button id="spin-btn" onClick={handleSpinClick}>
-        SPIN
+      <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 font-bold rounded w-52" onClick={handleSpinClick}>
+        {spinStatus ? "STOP" : "SPIN"}
       </button>
     </>
   )
